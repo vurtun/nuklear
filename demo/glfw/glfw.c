bf493a550e3365b2de2a68e9f3d286d79f843a22
vurtun <polygone@gmx.net>
2015-11-25 18:12:58 +0100

    added glfw demo

---
+++ demo/glfw/glfw.c
@@ -0,0 +1,540 @@
+/*
+    Copyright (c) 2015 Micha Mettke
+
+    This software is provided 'as-is', without any express or implied
+    warranty.  In no event will the authors be held liable for any damages
+    arising from the use of this software.
+
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
+
+    1.  The origin of this software must not be misrepresented; you must not
+        claim that you wrote the original software. If you use this software
+        in a product, an acknowledgment in the product documentation would be
+        appreciated but is not required.
+    2.  Altered source versions must be plainly marked as such, and must not be
+        misrepresented as being the original software.
+    3.  This notice may not be removed or altered from any source distribution.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <math.h>
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GLFW/glfw3.h>
+
+/* macros */
+#define DTIME       33
+#define MAX_DRAW_COMMAND_MEMORY (4 * 1024)
+#define MAX_VERTEX_MEMORY 128 * 1024
+#define MAX_ELEMENT_MEMORY 64 * 1024
+
+#include "../../zahnrad.h"
+
+
+static GLFWwindow *win;
+static void clipboard_set(const char *text){glfwSetClipboardString(win, text);}
+static int clipboard_is_filled(void) {return glfwGetClipboardString(win) != NULL;}
+static const char* clipboard_get(void){return glfwGetClipboardString(win);}
+
+#include "../demo.c"
+
+/* FUCK CALLBACKS */
+static int mouse_pos_x = 0;
+static int mouse_pos_y = 0;
+static struct demo gui;
+
+/* ==============================================================
+ *
+ *                      Utility
+ *
+ * ===============================================================*/
+static void
+die(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    fputs("\n", stderr);
+    exit(EXIT_FAILURE);
+}
+
+static char*
+file_load(const char* path, size_t* siz)
+{
+    char *buf;
+    FILE *fd = fopen(path, "rb");
+    if (!fd) die("Failed to open file: %s\n", path);
+    fseek(fd, 0, SEEK_END);
+    *siz = (size_t)ftell(fd);
+    fseek(fd, 0, SEEK_SET);
+    buf = (char*)calloc(*siz, 1);
+    fread(buf, *siz, 1, fd);
+    fclose(fd);
+    return buf;
+}
+
+struct device {
+    GLuint vbo, vao, ebo;
+    GLuint prog;
+    GLuint vert_shdr;
+    GLuint frag_shdr;
+    GLint attrib_pos;
+    GLint attrib_uv;
+    GLint attrib_col;
+    GLint uniform_tex;
+    GLint uniform_proj;
+    GLuint font_tex;
+    struct zr_draw_null_texture null;
+    struct zr_buffer cmds;
+};
+
+static void
+device_init(struct device *dev)
+{
+    GLint status;
+    static const GLchar *vertex_shader =
+        "#version 300 es\n"
+        "uniform mat4 ProjMtx;\n"
+        "in vec2 Position;\n"
+        "in vec2 TexCoord;\n"
+        "in vec4 Color;\n"
+        "out vec2 Frag_UV;\n"
+        "out vec4 Frag_Color;\n"
+        "void main() {\n"
+        "   Frag_UV = TexCoord;\n"
+        "   Frag_Color = Color;\n"
+        "   gl_Position = ProjMtx * vec4(Position.xy, 0, 1);\n"
+        "}\n";
+    static const GLchar *fragment_shader =
+        "#version 300 es\n"
+        "precision mediump float;\n"
+        "uniform sampler2D Texture;\n"
+        "in vec2 Frag_UV;\n"
+        "in vec4 Frag_Color;\n"
+        "out vec4 Out_Color;\n"
+        "void main(){\n"
+        "   Out_Color = Frag_Color * texture(Texture, Frag_UV.st);\n"
+        "}\n";
+
+    dev->prog = glCreateProgram();
+    dev->vert_shdr = glCreateShader(GL_VERTEX_SHADER);
+    dev->frag_shdr = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(dev->vert_shdr, 1, &vertex_shader, 0);
+    glShaderSource(dev->frag_shdr, 1, &fragment_shader, 0);
+    glCompileShader(dev->vert_shdr);
+    glCompileShader(dev->frag_shdr);
+    glGetShaderiv(dev->vert_shdr, GL_COMPILE_STATUS, &status);
+    assert(status == GL_TRUE);
+    glGetShaderiv(dev->frag_shdr, GL_COMPILE_STATUS, &status);
+    assert(status == GL_TRUE);
+    glAttachShader(dev->prog, dev->vert_shdr);
+    glAttachShader(dev->prog, dev->frag_shdr);
+    glLinkProgram(dev->prog);
+    glGetProgramiv(dev->prog, GL_LINK_STATUS, &status);
+    assert(status == GL_TRUE);
+
+    dev->uniform_tex = glGetUniformLocation(dev->prog, "Texture");
+    dev->uniform_proj = glGetUniformLocation(dev->prog, "ProjMtx");
+    dev->attrib_pos = glGetAttribLocation(dev->prog, "Position");
+    dev->attrib_uv = glGetAttribLocation(dev->prog, "TexCoord");
+    dev->attrib_col = glGetAttribLocation(dev->prog, "Color");
+
+    {
+        /* buffer setup */
+        GLsizei vs = sizeof(struct zr_draw_vertex);
+        size_t vp = offsetof(struct zr_draw_vertex, position);
+        size_t vt = offsetof(struct zr_draw_vertex, uv);
+        size_t vc = offsetof(struct zr_draw_vertex, col);
+
+        glGenBuffers(1, &dev->vbo);
+        glGenBuffers(1, &dev->ebo);
+        glGenVertexArrays(1, &dev->vao);
+
+        glBindVertexArray(dev->vao);
+        glBindBuffer(GL_ARRAY_BUFFER, dev->vbo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, dev->ebo);
+
+        glEnableVertexAttribArray((GLuint)dev->attrib_pos);
+        glEnableVertexAttribArray((GLuint)dev->attrib_uv);
+        glEnableVertexAttribArray((GLuint)dev->attrib_col);
+
+        glVertexAttribPointer((GLuint)dev->attrib_pos, 2, GL_FLOAT, GL_FALSE, vs, (void*)vp);
+        glVertexAttribPointer((GLuint)dev->attrib_uv, 2, GL_FLOAT, GL_FALSE, vs, (void*)vt);
+        glVertexAttribPointer((GLuint)dev->attrib_col, 4, GL_UNSIGNED_BYTE, GL_TRUE, vs, (void*)vc);
+    }
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+static struct zr_user_font
+font_bake_and_upload(struct device *dev, struct zr_font *font,
+    const char *path, unsigned int font_height, const zr_rune *range)
+{
+    int glyph_count;
+    int img_width, img_height;
+    struct zr_font_glyph *glyphes;
+    struct zr_baked_font baked_font;
+    struct zr_user_font user_font;
+    struct zr_recti custom;
+
+    memset(&baked_font, 0, sizeof(baked_font));
+    memset(&user_font, 0, sizeof(user_font));
+    memset(&custom, 0, sizeof(custom));
+
+    {
+        /* bake and upload font texture */
+        void *img, *tmp;
+        size_t ttf_size;
+        size_t tmp_size, img_size;
+        const char *custom_data = "....";
+        struct zr_font_config config;
+        char *ttf_blob = file_load(path, &ttf_size);
+        if (!ttf_blob)
+            die("[Font]: %s is not a file or cannot be found!\n", path);
+
+        /* setup font configuration */
+        memset(&config, 0, sizeof(config));
+        config.ttf_blob = ttf_blob;
+        config.ttf_size = ttf_size;
+        config.font = &baked_font;
+        config.coord_type = ZR_COORD_UV;
+        config.range = range;
+        config.pixel_snap = zr_false;
+        config.size = (float)font_height;
+        config.spacing = zr_vec2(0,0);
+        config.oversample_h = 1;
+        config.oversample_v = 1;
+
+        /* query needed amount of memory for the font baking process */
+        zr_font_bake_memory(&tmp_size, &glyph_count, &config, 1);
+        glyphes = (struct zr_font_glyph*)calloc(sizeof(struct zr_font_glyph), (size_t)glyph_count);
+        tmp = calloc(1, tmp_size);
+
+        /* pack all glyphes and return needed image width, height and memory size*/
+        custom.w = 2; custom.h = 2;
+        if (!zr_font_bake_pack(&img_size, &img_width,&img_height,&custom,tmp,tmp_size,&config, 1))
+            die("[Font]: failed to load font!\n");
+
+        /* bake all glyphes and custom white pixel into image */
+        img = calloc(1, img_size);
+        zr_font_bake(img, img_width, img_height, tmp, tmp_size, glyphes, glyph_count, &config, 1);
+        zr_font_bake_custom_data(img, img_width, img_height, custom, custom_data, 2, 2, '.', 'X');
+        {
+            /* convert alpha8 image into rgba8 image */
+            void *img_rgba = calloc(4, (size_t)(img_height * img_width));
+            zr_font_bake_convert(img_rgba, img_width, img_height, img);
+            free(img);
+            img = img_rgba;
+        }
+        {
+            /* upload baked font image */
+            glGenTextures(1, &dev->font_tex);
+            glBindTexture(GL_TEXTURE_2D, dev->font_tex);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (GLsizei)img_width, (GLsizei)img_height, 0,
+                        GL_RGBA, GL_UNSIGNED_BYTE, img);
+        }
+        free(ttf_blob);
+        free(tmp);
+        free(img);
+    }
+
+    /* default white pixel in a texture which is needed to draw primitives */
+    dev->null.texture.id = (int)dev->font_tex;
+    dev->null.uv = zr_vec2((custom.x + 0.5f)/(float)img_width,
+                            (custom.y + 0.5f)/(float)img_height);
+
+    /* setup font with glyphes. IMPORTANT: the font only references the glyphes
+      this was done to have the possibility to have multible fonts with one
+      total glyph array. Not quite sure if it is a good thing since the
+      glyphes have to be freed as well. */
+    zr_font_init(font, (float)font_height, '?', glyphes, &baked_font, dev->null.texture);
+    user_font = zr_font_ref(font);
+    return user_font;
+}
+
+static void
+device_shutdown(struct device *dev)
+{
+    glDetachShader(dev->prog, dev->vert_shdr);
+    glDetachShader(dev->prog, dev->frag_shdr);
+    glDeleteShader(dev->vert_shdr);
+    glDeleteShader(dev->frag_shdr);
+    glDeleteProgram(dev->prog);
+    glDeleteTextures(1, &dev->font_tex);
+    glDeleteBuffers(1, &dev->vbo);
+    glDeleteBuffers(1, &dev->ebo);
+}
+
+/* this is stupid but needed for C89 since sinf and cosf do not exist */
+static float fsin(float f) {return (float)sin(f);}
+static float fcos(float f) {return (float)cos(f);}
+
+static void
+device_draw(struct device *dev, struct zr_command_queue *queue, int width, int height,
+    enum zr_anti_aliasing AA)
+{
+    GLint last_prog, last_tex;
+    GLint last_ebo, last_vbo, last_vao;
+    GLfloat ortho[4][4] = {
+        {2.0f, 0.0f, 0.0f, 0.0f},
+        {0.0f,-2.0f, 0.0f, 0.0f},
+        {0.0f, 0.0f,-1.0f, 0.0f},
+        {-1.0f,1.0f, 0.0f, 1.0f},
+    };
+    ortho[0][0] /= (GLfloat)width;
+    ortho[1][1] /= (GLfloat)height;
+
+    /* save previous opengl state */
+    glGetIntegerv(GL_CURRENT_PROGRAM, &last_prog);
+    glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_tex);
+    glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &last_vao);
+    glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, &last_ebo);
+    glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &last_vbo);
+
+    /* setup global state */
+    glViewport(0, 0, width, height);
+    glEnable(GL_BLEND);
+    glBlendEquation(GL_FUNC_ADD);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_SCISSOR_TEST);
+    glActiveTexture(GL_TEXTURE0);
+
+    /* setup program */
+    glUseProgram(dev->prog);
+    glUniform1i(dev->uniform_tex, 0);
+    glUniformMatrix4fv(dev->uniform_proj, 1, GL_FALSE, &ortho[0][0]);
+
+    {
+        /* convert from command queue into draw list and draw to screen */
+        struct zr_draw_list draw_list;
+        const struct zr_draw_command *cmd;
+        void *vertexes, *elements;
+        const zr_draw_index *offset = NULL;
+
+        /* allocate vertex and element buffer */
+        memset(&draw_list, 0, sizeof(draw_list));
+        glBindVertexArray(dev->vao);
+        glBindBuffer(GL_ARRAY_BUFFER, dev->vbo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, dev->ebo);
+
+        glBufferData(GL_ARRAY_BUFFER, MAX_VERTEX_MEMORY, NULL, GL_STREAM_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, MAX_ELEMENT_MEMORY, NULL, GL_STREAM_DRAW);
+
+        /* load draw vertexes & elements directly into vertex + element buffer */
+        vertexes = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+        elements = glMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY);
+        {
+            struct zr_buffer vbuf, ebuf;
+            zr_buffer_init_fixed(&vbuf, vertexes, MAX_VERTEX_MEMORY);
+            zr_buffer_init_fixed(&ebuf, elements, MAX_ELEMENT_MEMORY);
+            zr_draw_list_init(&draw_list, &dev->cmds, &vbuf, &ebuf,
+                fsin, fcos, dev->null, AA);
+            zr_draw_list_load(&draw_list, queue, 1.0f, 22);
+        }
+        glUnmapBuffer(GL_ARRAY_BUFFER);
+        glUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
+
+        /* iterate over and execute each draw command */
+        zr_foreach_draw_command(cmd, &draw_list) {
+            if (!cmd->elem_count) continue;
+            glBindTexture(GL_TEXTURE_2D, (GLuint)cmd->texture.id);
+            glScissor((GLint)cmd->clip_rect.x,
+                height - (GLint)(cmd->clip_rect.y + cmd->clip_rect.h),
+                (GLint)cmd->clip_rect.w, (GLint)cmd->clip_rect.h);
+            glDrawElements(GL_TRIANGLES, (GLsizei)cmd->elem_count, GL_UNSIGNED_SHORT, offset);
+            offset += cmd->elem_count;
+        }
+
+        zr_command_queue_clear(queue);
+        zr_draw_list_clear(&draw_list);
+    }
+
+    /* restore old state */
+    glUseProgram((GLuint)last_prog);
+    glBindTexture(GL_TEXTURE_2D, (GLuint)last_tex);
+    glBindBuffer(GL_ARRAY_BUFFER, (GLuint)last_vbo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, (GLuint)last_ebo);
+    glBindVertexArray((GLuint)last_vao);
+    glDisable(GL_SCISSOR_TEST);
+}
+
+static void
+error_callback(int error, const char *description)
+{
+    fprintf(stderr, "Error %d: %s\n", error, description);
+}
+
+static void
+input_key(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    int down = action == GLFW_PRESS;
+    UNUSED(window);
+    UNUSED(scancode);
+    if (key == GLFW_KEY_RIGHT_SHIFT || key == GLFW_KEY_LEFT_SHIFT)
+        zr_input_key(&gui.input, ZR_KEY_SHIFT, down);
+    else if (key == GLFW_KEY_DELETE)
+        zr_input_key(&gui.input, ZR_KEY_DEL, down);
+    else if (key == GLFW_KEY_ENTER)
+        zr_input_key(&gui.input, ZR_KEY_ENTER, down);
+    else if (key == GLFW_KEY_BACKSPACE)
+        zr_input_key(&gui.input, ZR_KEY_BACKSPACE, down);
+    else if (key == GLFW_KEY_LEFT)
+        zr_input_key(&gui.input, ZR_KEY_LEFT, down);
+    else if (key == GLFW_KEY_RIGHT)
+        zr_input_key(&gui.input, ZR_KEY_RIGHT, down);
+    else if (key == GLFW_KEY_C)
+        zr_input_key(&gui.input, ZR_KEY_COPY, down && (mods & GLFW_MOD_CONTROL));
+    else if (key == GLFW_KEY_V)
+        zr_input_key(&gui.input, ZR_KEY_PASTE, down && (mods & GLFW_MOD_CONTROL));
+    else if (key == GLFW_KEY_X)
+        zr_input_key(&gui.input, ZR_KEY_CUT, down && (mods & GLFW_MOD_CONTROL));
+}
+
+static void
+input_motion(GLFWwindow *window, double xpos, double ypos)
+{
+    const int x = (int)xpos;
+    const int y = (int)ypos;
+    UNUSED(window);
+    mouse_pos_x = x;
+    mouse_pos_y = y;
+    zr_input_motion(&gui.input, x, y);
+}
+
+static void
+input_button(GLFWwindow *window, int button, int action, int mods)
+{
+    struct zr_input *in = &gui.input;
+    int x = mouse_pos_x;
+    int y = mouse_pos_y;
+    UNUSED(window);
+    UNUSED(mods);
+    if (button == 0)
+        zr_input_button(in, ZR_BUTTON_LEFT, x, y, action == GLFW_PRESS);
+    if (button == 1)
+        zr_input_button(in, ZR_BUTTON_RIGHT, x, y, action == GLFW_PRESS);
+}
+
+static void
+input_text(GLFWwindow *window, unsigned int codepoint)
+{
+    UNUSED(window);
+    zr_input_unicode(&gui.input, codepoint);
+}
+
+static void
+input_scroll(GLFWwindow *window, double xoffset, double yoffset)
+{
+    UNUSED(window);
+    UNUSED(xoffset);
+    zr_input_scroll(&gui.input, (float)yoffset);
+}
+
+static void* mem_alloc(zr_handle unused, size_t size)
+{UNUSED(unused); return calloc(1, size);}
+static void mem_free(zr_handle unused, void *ptr)
+{UNUSED(unused); free(ptr);}
+
+int
+main(int argc, char *argv[])
+{
+    /* Platform */
+    const char *font_path;
+    int win_width, win_height;
+    int width = 0, height = 0;
+
+    /* GUI */
+    struct zr_allocator alloc;
+    struct device device;
+    struct zr_font font;
+
+    font_path = argv[1];
+    if (argc < 2)
+        die("Missing TTF Font file argument!");
+
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        fprintf(stdout, "[GFLW] failed to init!\n");
+        return 1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Demo", NULL, NULL);
+    glfwMakeContextCurrent(win);
+
+    glfwSetCursorPosCallback(win, input_motion);
+    glfwSetMouseButtonCallback(win, input_button);
+    glfwSetKeyCallback(win, input_key);
+    glfwSetCharCallback(win, input_text);
+    glfwSetScrollCallback(win, input_scroll);
+
+    /* OpenGL */
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glewExperimental = 1;
+    if (glewInit() != GLEW_OK)
+        die("Failed to setup GLEW\n");
+
+    /* GUI */
+    alloc.userdata.ptr = NULL;
+    alloc.alloc = mem_alloc;
+    alloc.free = mem_free;
+    memset(&gui, 0, sizeof gui);
+    zr_buffer_init(&device.cmds, &alloc, 1024, 2.0f);
+    zr_command_queue_init(&gui.queue, &alloc, 1024, 2.0f);
+    gui.font = font_bake_and_upload(&device, &font, font_path, 14,
+                                    zr_font_default_glyph_ranges());
+
+    init_demo(&gui);
+    device_init(&device);
+
+    while (!glfwWindowShouldClose(win) && gui.running) {
+        /* Input */
+        zr_input_begin(&gui.input);
+        glfwPollEvents();
+        zr_input_end(&gui.input);
+
+        /* GUI */
+        glfwGetWindowSize(win, &width, &height);
+        gui.w = (size_t)width;
+        gui.h = (size_t)height;
+        run_demo(&gui);
+
+        /* Draw */
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
+        device_draw(&device, &gui.queue, width, height, ZR_ANTI_ALIASING_ON);
+        glfwSwapBuffers(win);
+    }
+
+cleanup:
+    /* Cleanup */
+    free(font.glyphes);
+    zr_command_queue_free(&gui.queue);
+    zr_buffer_free(&device.cmds);
+    device_shutdown(&device);
+    glfwTerminate();
+    return 0;
+}
+
