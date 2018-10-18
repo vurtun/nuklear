3121e6cb2a1ae9e490e6fae5129bdafdf5f1dda8
Kenney Phillis Jr <kphillisjr@gmail.com>
2018-10-18 18:09:29 -0500

    gdi demo: Implement nk_gdi_rect_multi_color.
    This implements the GDI Rect Multicolor functions. This will work without
    any problems when targetting Windows 2000 and up.
    
    TODO: Fix Alpha blending. The Color Matrix does not render correctly.

--- demo/gdi/build.bat
+++ demo/gdi/build.bat
@@ -3,4 +3,4 @@
 rem This will use VS2015 for compiler
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 
-cl /nologo /W3 /O2 /fp:fast /Gm- /Fedemo.exe main.c user32.lib gdi32.lib /link /incremental:no
+cl /nologo /W3 /O2 /fp:fast /Gm- /Fedemo.exe main.c user32.lib gdi32.lib Msimg32.lib /link /incremental:no
