7fafcb3ac741cd55b22084d2ba08d117e44b86d9
Jimmi Holst Christensen <jhc@dismail.de>
2019-09-18 18:27:25 +0200

    add ctrl support for x11 demos

--- demo/x11_opengl3/nuklear_xlib_gl3.h
+++ demo/x11_opengl3/nuklear_xlib_gl3.h
@@ -618,6 +618,7 @@ nk_x11_handle_event(XEvent *evt)
         int ret, down = (evt->type == KeyPress);
         KeySym *code = XGetKeyboardMapping(x11.dpy, (KeyCode)evt->xkey.keycode, 1, &ret);
         if (*code == XK_Shift_L || *code == XK_Shift_R) nk_input_key(ctx, NK_KEY_SHIFT, down);
+        else if (*code == XK_Control_L || *code == XK_Control_R) nk_input_key(ctx, NK_KEY_CTRL, down);
         else if (*code == XK_Delete)    nk_input_key(ctx, NK_KEY_DEL, down);
         else if (*code == XK_Return)    nk_input_key(ctx, NK_KEY_ENTER, down);
         else if (*code == XK_Tab)       nk_input_key(ctx, NK_KEY_TAB, down);
